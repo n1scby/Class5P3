516dca5f3f07a080fc9faa6be2df2fe0593f3c3d
Nic B <n1scby@yahoo.com>
2017-10-24 21:07:10 -0400

    initial commit

---
+++ match.js
@@ -0,0 +1,12 @@
+var highend = 100;
+var lowend = 1;
+for (var i=lowend; i<=highend; i++)
+{
+    if (i%3 == 0)
+    {
+        console.log('MATCH!');
+    }
+    else {
+    console.log(i);
+    }
+}
