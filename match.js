700ffd7140b2d3839c8057c10937e108d63c9fe0
Nic B <n1scby@yahoo.com>
2017-10-24 21:08:57 -0400

    Match on 5

--- match.js
+++ match.js
@@ -1,8 +1,9 @@
 var highend = 100;
 var lowend = 1;
+var matchNumber = 5;
 for (var i=lowend; i<=highend; i++)
 {
-    if (i%3 == 0)
+    if (i%matchNumber == 0)
     {
         console.log('MATCH!');
     }
